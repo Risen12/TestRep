612c9f337ee54c0912d401ef2e938cf8746c7233
Risen12 <den2033@bk.ru>
2022-08-13 15:51:42 +0300

    Taking data from server to class Informator.Also now i can add new data to Informator's State

--- src/Informator.js
+++ src/Informator.js
@@ -1,88 +1,37 @@
+import { current } from "@reduxjs/toolkit";
 import React from "react";
-import { connect } from "react-redux";
 import { UpdateData } from "./Store/Reducers/InfoSlice";
 
 class Informator extends React.Component
 {
-    constructor()
-    {
-        super();
-        this.currentState = {};
-    }
+    constructor(props){ 
+        super(props);
+        this.state = {info:{}}; 
+        this.updateState = this.updateState.bind(this);
+      };
+        
 
+    updateState =(NewState) => 
+    {
+        this.setState({info:NewState}, function(){console.log(this.state)});
+    };
+
+    componentDidMount()
+    {
+        let url = "https://subabonent.ru/helen/api100.php";
+        console.log(this.state);
+
+        fetch(url)
+        .then(response => response.json())
+        .then(data => this.updateState(data));
+
+    }; 
 
     render()
     {
-        return null;
-    };
-    componentDidMount()
-    {
-        
-        function Request()
-        {
-            let url = "https://subabonent.ru/helen/api100.php";
-            fetch(url)
-            .then(
-                function(response)
-                {
-                    if(response.status !== 200)
-                    {
-                        console.log("Some problem with connection. Response status is " + response.status);
-                        return null;
-                    }
-                    response.json().then(function(data)
-                    {
-                        let Initialstate = 
-                        {
-                            tv: data.data.values.tv,
-                            cm: data.data.values.km,
-                            cooler: data.data.values.fr,
-                            syrups: data.data.values.sp,
-                            glassM: data.data.values.dl,
-                            glassS: data.data.values.ds,
-                            water: data.data.values.wt,
-                            dwater: data.data.values.ws,
-                            garbage: data.data.values.gb
-                        };
-                        console.log(JSON.stringify(data.data.values));
-                        this.currentState = Initialstate;
-                        console.log(this.currentState);
-                        return data;
-                    }
-                    )
-                }
-            )
-            .catch(
-                function(err)
-                {
-                    console.log("Fetch error: " + err);
-                }
-            );
-        };
-        Request();
-        console.log(this.props.info);
-        const Update = () => UpdateData;
-        Update();
-        console.log(this.props.info);
+        return true;
     };
 
 };
 
-const MapStateToProps = (state) => {
-    return { 
-        info:state.info
-    }
-}
-
-// const mapDispatchToProps=(dispatch)=>({
-//    UpdateData:()=>dispatch(UpdateData(Informator.currentState))
-//   });
-/*   const mapDispatchToProps = (dispatch) => {
-    return {
-       UpdateData: () => dispatch(UpdateData())
-    }
-} */
-
-
-export default connect (MapStateToProps)(Informator);
-//export default Informator;
+export default Informator;
